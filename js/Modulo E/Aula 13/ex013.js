ccab1890d99e71a07b823b5bb4975a48f732c375
Aysllan Magalhães <aysllanaas@gmail.com>
2025-02-12 15:58:25 -0300

    aula 13-14

---
+++ js/Modulo E/Aula 13/ex013.js
@@ -0,0 +1,12 @@
+var a = 1
+console.log(`Quero uma contagem de ${a}`)
+while (a > -10001) {
+    console.log(`Aqui está ...${a}`)
+    a = a - 10
+}
+
+var b = 1
+do {
+    console.log(`A Passo ${b}`)
+    b++
+} while (b < 10)
