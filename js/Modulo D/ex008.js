70a21a5dac96420e9018cc8baec785086e1f4787
Aysllan Magalhães <aysllanaas@gmail.com>
2025-02-12 15:25:36 -0300

    aula 10

---
+++ js/Modulo D/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 30
+if (velocidade > 0 && velocidade < 40) {
+    console.log(`Sua velocidade está ideal para a via frequentada, continue assim`);
+} else {
+    console.log(`Sua velocidade de ${velocidade} exede o permitido você recebeu uma multa`);
+}
---
+++ js/Modulo D/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Você esta vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Você não é estrangeiro')
+}else{
+    console.log('Você é estrangeiro')
+}
