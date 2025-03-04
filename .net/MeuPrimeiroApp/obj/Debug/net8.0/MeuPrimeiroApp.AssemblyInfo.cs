c619f36e26d2a44c1e7610523c37845543109161
Aysllan Magalhães <aysllanaas@gmail.com>
2025-03-04 14:08:02 -0300

    .

--- .net/MeuPrimeiroApp/obj/Debug/net8.0/MeuPrimeiroApp.AssemblyInfo.cs
+++ .net/MeuPrimeiroApp/obj/Debug/net8.0/MeuPrimeiroApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuPrimeiroApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ed5a6178b0b675cc2dc19e2d0353d85041b2cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca8d551d42f9b024be79a976a799cda26a3377d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuPrimeiroApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuPrimeiroApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
