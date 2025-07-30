4edbcfa33156b225c0585c816dd54930a8f919df
Aysllan Magalhães <aysllanaas@gmail.com>
2025-07-30 14:53:30 -0300

    a

---
+++ vue js/instalacao/src/app.js
@@ -0,0 +1,10 @@
+const MyNameApp = {
+    data() {
+        return {
+            name: 'Proddyt',
+        };
+    }
+}
+
+// Create the Vue application and mount it to the DOM
+Vue.createApp(MyNameApp).mount("#app");
